25955443ab42b44805f0519c38bbc5b1ecb27029
VS <solonin.vadim@mail.ru>
2024-08-16 22:08:02 +0300

    first step to adding page object

---
+++ pages/QametaIoMainPage.ts
@@ -0,0 +1,35 @@
+import { type Locator, Page } from '@playwright/test';
+
+export default class QametaIoMainPage { 
+    readonly page: Page;
+    readonly featuresNavLink: Locator;
+    readonly freeTrialButton: Locator;
+    readonly sandboxButton: Locator;
+    readonly cloudTrialPageTitle : Locator;
+    readonly inputEmail : Locator;
+
+    constructor(page: Page){
+        this.page = page;
+        this.featuresNavLink = page.locator("#features_nav_desctop");
+        this.freeTrialButton = page.locator("#free_trial_pricing");
+        this.sandboxButton = page.locator('#sandbox');
+        this.cloudTrialPageTitle = page.locator("//h1['font-extrabold']");
+        this.inputEmail = page.locator('[data-testid="input__email"]');
+    }
+
+    async hoverFeaturesNavLink(){
+        await this.featuresNavLink.hover();
+    }
+
+    async clickFeaturesNavLink(text : string){
+        await this.page.locator('#features_nav_desctop :text(" ' + text + '")').click();
+    }
+    
+    async clickFreeTrialButton(){
+        await this.freeTrialButton.click();
+    }
+    
+    async clickSandboxButton(){
+        await this.sandboxButton.click();
+    }
+}
--- tests/QametaIoMainPageTests.spec.ts
+++ tests/QametaIoMainPageTests.spec.ts
@@ -1,29 +1,29 @@
-import { request } from 'http';
-
-const { test, expect } = require('@playwright/test');
+import QametaIoMainPage from '../pages/QametaIoMainPage';
+import test, { expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://qameta.io/');
 });
 
 test('Open continuous testing page', async ({ page }) => {
-    await page.locator("#features_nav_desctop").hover()
-    await page.locator('#features_nav_desctop :text("Continuous Testing")').click();
-    await expect(page).toHaveTitle("Continuous testing with comprehensive CI/CD Integrations");
-    await expect(page).toHaveURL("https://qameta.io/ci-integrations/");
+  const qametaIoMainPage = new QametaIoMainPage(page);
+  qametaIoMainPage.hoverFeaturesNavLink();
+  qametaIoMainPage.clickFeaturesNavLink("Continuous Testing");
+  await expect(page).toHaveTitle("Continuous testing with comprehensive CI/CD Integrations");
+  await expect(page).toHaveURL("https://qameta.io/ci-integrations/");
 });
 
 test('Open cloud trial page', async ({ page }) => {
-  await page.locator('#free_trial_pricing').click();
+  const qametaIoMainPage = new QametaIoMainPage(page);
+  qametaIoMainPage.clickFreeTrialButton();
   await expect(page).toHaveTitle("Allure TestOps: Centralized Test Reporting");
   await expect(page).toHaveURL("https://qameta.io/cloud-trial-request/");
-  let cloudTrialPageTitle = page.locator("//h1['font-extrabold']")
-  await expect(cloudTrialPageTitle).toHaveText("Try out Allure TestOps Cloud free for 14 days to speed up your testing")
+  await expect(qametaIoMainPage.cloudTrialPageTitle).toHaveText("Try out Allure TestOps Cloud free for 14 days to speed up your testing")
 });
 
 test('Open sandbox page', async ({ page }) => {
-  await page.locator('#sandbox').click();
+  const qametaIoMainPage = new QametaIoMainPage(page);
+  qametaIoMainPage.clickSandboxButton();
   await expect(page).toHaveTitle("Allure TestOps");
-  let inputEmail = page.locator('[data-testid="input__email"]')
-  await expect(inputEmail).toBeVisible();
+  await expect(qametaIoMainPage.inputEmail).toBeVisible();
 });
